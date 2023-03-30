b470edc448275899e2e72768965a29f40bfd6909
mikegyamfi <codeage20@gmail.com>
2023-03-30 22:28:00 +0000

    changes to auth templates

--- shares_app/views.py
+++ shares_app/views.py
@@ -74,6 +74,8 @@ def send_bundle_page(request):
                 new_transaction.save()
                 messages.success(request, "Transaction Successful.")
                 receiver_message = f"Your bundle purchase has been completed successfully. {amount}MB has been credited to you.\nReference: {batch_id}\n"
+                print(current_user.sms_sender_name)
+                print(receiver)
                 sms_url = f"https://sms.arkesel.com/sms/api?action=send-sms&api_key=UmpEc1JzeFV4cERKTWxUWktqZEs&to={receiver}&from={current_user.sms_sender_name}&sms={receiver_message}"
                 response = requests.request("GET", url=sms_url)
                 print(response.status_code)
