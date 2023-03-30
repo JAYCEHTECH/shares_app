58a590b362f257350724a64a3127ce50a15fad66
mikegyamfi <codeage20@gmail.com>
2023-03-30 23:06:38 +0000

    changes to auth templates

--- shares_app/views.py
+++ shares_app/views.py
@@ -43,7 +43,7 @@ def user_profile(request):
 def send_bundle_page(request):
     if request.method == "POST":
         receiver = request.POST.get("phone")
-        string_r = str(receiver)
+        string_r = receiver[3:]
         amount = int(request.POST.get("amount"))
 
         reference = f"BPS{secrets.token_hex(3)}".upper()
@@ -75,7 +75,7 @@ def send_bundle_page(request):
                 new_transaction.save()
                 messages.success(request, "Transaction Successful.")
                 receiver_message = f"Your bundle purchase has been completed successfully. {amount}MB has been credited to you.\nReference: {batch_id}\n"
-                sms_url = f"https://sms.arkesel.com/sms/api?action=send-sms&api_key=UmpEc1JzeFV4cERKTWxUWktqZEs&to={string_r}&from=BP-Bundle&sms={receiver_message}"
+                sms_url = f"https://sms.arkesel.com/sms/api?action=send-sms&api_key=UmpEc1JzeFV4cERKTWxUWktqZEs&to=0{string_r}&from=BP-Bundle&sms={receiver_message}"
                 response = requests.request("GET", url=sms_url)
                 print(response.status_code)
                 print(response.text)
