178907c221f40ebda39092f8f7d4e77cb8751be5
mikegyamfi <codeage20@gmail.com>
2023-03-31 14:10:11 +0000

    changes to sign up form

--- shares_app/views.py
+++ shares_app/views.py
@@ -43,63 +43,67 @@ def user_profile(request):
 def send_bundle_page(request):
     if request.method == "POST":
         receiver = request.POST.get("phone")
-        print(receiver)
-        amount = int(request.POST.get("amount"))
-
-        reference = f"BPS{secrets.token_hex(3)}".upper()
-
-        current_user = models.UserProfile.objects.filter(user=request.user).first()
-
-        if amount > int(current_user.bundle_amount):
-            messages.error(request, "Not enough bundle to send")
-        elif int(current_user.bundle_amount) <= 0:
-            messages.error(request, "Kindly top up", "error")
+        if len(receiver) != 10:
+            messages.error(request, "Number must be 10 digits")
+            return redirect('send_bundle_page')
         else:
-            send_bundle = helper.send_flexi_bundle(request.user, current_user, receiver, amount)
-            response = send_bundle["response"]
-            json_data = send_bundle["json_data"]
-            batch_id = json_data["batchId"]
+            print(receiver)
+            amount = int(request.POST.get("amount"))
 
-            if response.status_code == 200:
-                current_user.bundle_amount -= amount
-                current_user.save()
+            reference = f"BPS{secrets.token_hex(3)}".upper()
 
-                new_transaction = models.TransactionHistory.objects.create(
-                    user=request.user,
-                    beneficiary=receiver,
-                    bundle_amount=amount,
-                    transaction_status="Success",
-                    reference=reference,
-                    batch_id=batch_id
-                )
-                new_transaction.save()
-                print(current_user.sms_sender_name)
-                messages.success(request, "Transaction Successful.")
-                receiver_message = f"Your bundle purchase has been completed successfully. {amount}MB has been credited to you.\nReference: {batch_id}\n"
-                quicksend_url = "https://uellosend.com/quicksend/"
-                data = {
-                    'api_key': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.=eyJkYXRhIjp7InVzZXJpZCI6MTU5MiwiYXBpU2VjcmV0IjoiaFY2YjNDcHR1PW9wQnB2IiwiaXNzdWVyIjoiVUVMTE9TRU5EIn19',
-                    'sender_id': current_user.sms_sender_name,
-                    'message': receiver_message,
-                    'recipient': receiver
-                }
+            current_user = models.UserProfile.objects.filter(user=request.user).first()
 
-                headers = {'Content-type': 'application/json'}
-
-                response = requests.post(quicksend_url, headers=headers, json=data)
-                print(response.json())
-
+            if amount > int(current_user.bundle_amount):
+                messages.error(request, "Not enough bundle to send")
+            elif int(current_user.bundle_amount) <= 0:
+                messages.error(request, "Kindly top up", "error")
             else:
-                new_transaction = models.TransactionHistory.objects.create(
-                    user=request.user,
-                    beneficiary=receiver,
-                    bundle_amount=amount,
-                    transaction_status="Failed",
-                    reference=reference,
-                    batch_id=batch_id,
-                )
-                new_transaction.save()
-                messages.error(request, "Transaction Failed")
+                send_bundle = helper.send_flexi_bundle(request.user, current_user, receiver, amount)
+                response = send_bundle["response"]
+                json_data = send_bundle["json_data"]
+                batch_id = json_data["batchId"]
+
+                if response.status_code == 200:
+                    current_user.bundle_amount -= amount
+                    current_user.save()
+
+                    new_transaction = models.TransactionHistory.objects.create(
+                        user=request.user,
+                        beneficiary=receiver,
+                        bundle_amount=amount,
+                        transaction_status="Success",
+                        reference=reference,
+                        batch_id=batch_id
+                    )
+                    new_transaction.save()
+                    print(current_user.sms_sender_name)
+                    messages.success(request, "Transaction Successful.")
+                    receiver_message = f"Your bundle purchase has been completed successfully. {amount}MB has been credited to you.\nReference: {batch_id}\n"
+                    quicksend_url = "https://uellosend.com/quicksend/"
+                    data = {
+                        'api_key': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.=eyJkYXRhIjp7InVzZXJpZCI6MTU5MiwiYXBpU2VjcmV0IjoiaFY2YjNDcHR1PW9wQnB2IiwiaXNzdWVyIjoiVUVMTE9TRU5EIn19',
+                        'sender_id': current_user.sms_sender_name,
+                        'message': receiver_message,
+                        'recipient': receiver
+                    }
+
+                    headers = {'Content-type': 'application/json'}
+
+                    response = requests.post(quicksend_url, headers=headers, json=data)
+                    print(response.json())
+
+                else:
+                    new_transaction = models.TransactionHistory.objects.create(
+                        user=request.user,
+                        beneficiary=receiver,
+                        bundle_amount=amount,
+                        transaction_status="Failed",
+                        reference=reference,
+                        batch_id=batch_id,
+                    )
+                    new_transaction.save()
+                    messages.error(request, "Transaction Failed")
     user_profile_data = models.UserProfile.objects.filter(user=request.user).first()
     context = {'data': user_profile_data}
 
