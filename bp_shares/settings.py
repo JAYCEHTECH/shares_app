841dd7138fb460ef5ef5d116fcbdb06cc9884263
mikegyamfi <codeage20@gmail.com>
2023-11-29 15:45:33 +0000

    cors

--- bp_shares/settings.py
+++ bp_shares/settings.py
@@ -72,17 +72,22 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
+
 CORS_ALLOWED_ORIGINS = [  # Replace with your allowed domain(s)
    '*'
+]
+
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^https://\w+\.example\.com$",
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
