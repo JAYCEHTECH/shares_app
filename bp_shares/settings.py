f2a0bc201b27b8f2d65dd991f61c6fd1cc4ee88d
mikegyamfi <codeage20@gmail.com>
2023-11-29 15:51:47 +0000

    cors

--- bp_shares/settings.py
+++ bp_shares/settings.py
@@ -86,6 +86,16 @@ CORS_ALLOWED_ORIGINS = [  # Replace with your allowed domain(s)
    '*'
 ]
 
+CORS_ALLOW_HEADERS = [
+    'Accept',
+    'Accept-Language',
+    'Content-Type',
+    'api-key',
+    'api-secret'
+    # Include other headers as needed
+    # Add more headers as required for your application
+]
+
 CORS_ALLOWED_ORIGIN_REGEXES = [
     r"^https://\w+\.example\.com$",
 ]
