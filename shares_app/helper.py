cfb9c059626e78dfd729bd82f30187ef6d8d5d29
JAYCEHTECH <78296113+JAYCEHTECH@users.noreply.github.com>
2024-09-01 12:19:18 +0000

    Update helper.py

--- shares_app/helper.py
+++ shares_app/helper.py
@@ -125,7 +125,7 @@ def api_send_bundle(data):
     receiver = data["receiver"],
     reference = f"{secrets.token_hex(6)}".upper()
 
-    print(receiver)
+    print(receiver.replace(',', '')
     print(receiver)
     print(receiver)
     print(receiver)
